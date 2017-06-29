752351fda3176010abed78d95a427e9dbdf41020
hugua <weifeng.chen@outlook.com>
2017-06-29 12:06:19 +0800

    springboot+dubbo

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+1 . springboot + dubbo + zookeeper 实现服务注册
+2. dubbo_interface是服务接口
+3. dubbo_registry 是注册服务
+4. dubbo_consumer 是使用服务
---
+++ dubbo_interface/src/main/java/com/chen/service/DemoService.java
@@ -0,0 +1,8 @@
+package com.chen.service;
+
+/**
+ * Created by cvter on 2017/6/23.
+ */
+public interface DemoService {
+    String sayHello(String name);
+}
